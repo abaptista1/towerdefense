d827cef914c177b62d0cbce949ecf20de953f8b5
abaptista1 <46967172+abaptista1@users.noreply.github.com>
2019-05-08 09:47:21 -0400

    tower defend

---
+++ TowerDefenseGame/Assets/BuildManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogError("more than one buildmanager");
+            return;
+        }
+        instance = this;
+      
+    }
+
+    public GameObject standardTurretPrefab;
+
+    void Start()
+    {
+        turretToBuild = standardTurretPrefab;
+    }
+
+    private GameObject turretToBuild;
+
+    public GameObject GetTurretToBuild ()
+    {
+        return turretToBuild;
+    }
+}
